557705a7147ffecb873b8269ca89275b35ae3fe5
Manuel Betancurt <m.betancurt@wisr.com.au>
2020-05-02 14:53:46 +1000

    android bridge

--- android/app/src/main/java/com/blunobridge/DeviceModule.java
+++ android/app/src/main/java/com/blunobridge/DeviceModule.java
@@ -1,27 +1,80 @@
 package com.blunobridge;
 
+import android.content.Intent;
+import android.widget.Toast;
+
 import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
 public class DeviceModule extends ReactContextBaseJavaModule {
+    private static ReactApplicationContext reactContext;
+
+    Conspi conspi;
+
     //constructor
     public DeviceModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        reactContext = reactContext;
     }
     //Mandatory function getName that specifies the module name
     @Override
     public String getName() {
         return "BLuno";
     }
+
+        //USEFUL for passing constants to JS
+//    @Override
+//    public Map<String, Object> getConstants() {
+//        final Map<String, Object> constants = new HashMap<>();
+//        constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
+//        constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
+//        return constants;
+//    }
+//
     //Custom function that we are going to export to JS
+//    @ReactMethod
+//    public void getDeviceName(Callback cb) {
+//        try{
+//            Conspi conspi = new Conspi();
+//
+//
+//            cb.invoke(null, conspi.chamad());
+//
+//        }catch (Exception e){
+//            cb.invoke(e.toString(), null);
+//        }
+//    }
+
     @ReactMethod
-    public void getDeviceName(Callback cb) {
-        try{
-            cb.invoke(null, android.os.Build.MODEL);
-        }catch (Exception e){
-            cb.invoke(e.toString(), null);
-        }
+    public void show(String message, int duration) {
+        Toast.makeText(getReactApplicationContext(), message, duration).show();
     }
+
+    @ReactMethod
+    public void initEvent(String params) {
+        conspi = new Conspi();
+
+//        conspi.onCreate();
+
+        conspi.initEvent_(params);
+    }
+
+    @ReactMethod
+    public void searchDeviceEvent(String params) {
+        conspi.searchDeviceEvent_(params);
+    }
+
+//    @ReactMethod
+//    public void searchDeviceEvent(Callback cb) {
+//        try{
+//            cb.invoke(null, conspi.searchDeviceEvent_());
+//
+//        }catch (Exception e){
+//            cb.invoke(e.toString(), null);
+//        }
+//    }
+
+
 }
